240d2fdcf1a4e5cd3e9c672210608d77a5260627
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-02-22 07:01:19 +0200

    Rick And Morty

--- lib/data/repository/charctersrepository.dart
+++ lib/data/repository/charctersrepository.dart
@@ -1,6 +1,6 @@
 import 'package:rickandmortycubit/data/models/charcters.dart';
-
 import '../Web_services/characters_web_services.dart';
+
 class CharactersRepository {
   final CharctersWebServices charactersWebServices;
 
@@ -8,6 +8,8 @@ class CharactersRepository {
 
   Future<List<Character>> getAllCharacters() async {
     final characters = await charactersWebServices.getAllCharacters();
-    return List<Character>.from((characters["results"] as List).map((e) => Character.fromJson(e)));
+
+    return List<Character>.from(
+        (characters["results"] as List).map((e) => Character.fromJson(e)));
   }
 }
